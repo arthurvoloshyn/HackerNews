8114eb45f2afe3e38ec016e39f72d6c3f6b80396
Artur <grandfaraon93@gmail.com>
2019-11-21 13:34:06 +0200

    Updates

---
+++ precache-manifest.13d899a904e1fd11e95bac1a6c1fff68.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d6520477d5db50b3cd2980e7ef91148",
+    "url": "/AlgoliaReactApi/index.html"
+  },
+  {
+    "revision": "f2bffff2dc24cfb301a4",
+    "url": "/AlgoliaReactApi/static/css/main.cdc1abe6.chunk.css"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/AlgoliaReactApi/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "f2bffff2dc24cfb301a4",
+    "url": "/AlgoliaReactApi/static/js/main.be45a9dc.chunk.js"
+  },
+  {
+    "revision": "1f6e1c2712f1d17a46b2",
+    "url": "/AlgoliaReactApi/static/js/runtime~main.393fd6d2.js"
+  }
+]);
