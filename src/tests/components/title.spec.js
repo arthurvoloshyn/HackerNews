9351b24adeee772064290295a44635d79779f6c9
ArturW1998 <grandfaraon93@gmail.com>
2020-08-24 20:28:16 +0300

    Prepared environment for testing. Created test for Title component.

---
+++ src/tests/components/title.spec.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Title from '../../components/title/title';
+
+describe('Title component', () => {
+  it('should render Title component with props', () => {
+    const component = shallow(<Title title="Test title" />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should render Title component without props', () => {
+    const component = shallow(<Title />);
+    expect(component).toMatchSnapshot();
+  });
+});
