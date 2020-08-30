8000501981398e775c800193a753cbfd98c9c5ee
ArturW1998 <grandfaraon93@gmail.com>
2020-08-30 20:43:45 +0300

    Updated the post test

--- src/tests/containers/posts.spec.js
+++ src/tests/containers/posts.spec.js
@@ -20,7 +20,7 @@ global.fetch = jest.fn().mockImplementation(() => mockFetchPromise);
 const setUp = () => shallow(<Posts />);
 
 describe('Posts component', () => {
-  const DEFAULT_PAGE = 10;
+  const INIT_PAGE = 10;
 
   const defaultSearchQuery = '';
   const defaultHitsPerPage = 20;
@@ -46,14 +46,14 @@ describe('Posts component', () => {
 
   describe('updatePage method', () => {
     it("should update state 'page' value", () => {
-      instance.updatePage(DEFAULT_PAGE);
-      expect(component.state().page).toBe(DEFAULT_PAGE);
+      instance.updatePage(INIT_PAGE);
+      expect(component.state().page).toBe(INIT_PAGE);
     });
 
     it('should call fetch with given argument', () => {
-      const SEARCH_PARAMS = `${SEARCH_PARAM}${defaultSearchQuery}&${PAGE_HITS}${defaultHitsPerPage}&${PAGE_PARAM}${DEFAULT_PAGE}`;
+      const SEARCH_PARAMS = `${SEARCH_PARAM}${defaultSearchQuery}&${PAGE_HITS}${defaultHitsPerPage}&${PAGE_PARAM}${INIT_PAGE}`;
 
-      instance.updatePage(DEFAULT_PAGE);
+      instance.updatePage(INIT_PAGE);
       expect(global.fetch).toHaveBeenCalledWith(`${BASE_PATH}${SEARCH_PATH}?${SEARCH_PARAMS}`);
     });
   });
@@ -61,7 +61,7 @@ describe('Posts component', () => {
   describe('handlePageChange method', () => {
     it("should call 'updatePage' with given argument", () => {
       instance.updatePage = jest.fn();
-      instance.setState({ page: DEFAULT_PAGE });
+      instance.setState({ page: INIT_PAGE });
       instance.handlePageChange({
         target: { getAttribute: jest.fn().mockReturnValue('1') },
       });
@@ -70,25 +70,25 @@ describe('Posts component', () => {
 
     it("should call 'updatePage' with increased value", () => {
       instance.updatePage = jest.fn();
-      instance.setState({ page: DEFAULT_PAGE });
+      instance.setState({ page: INIT_PAGE });
       instance.handlePageChange({
         target: { getAttribute: jest.fn().mockReturnValue(PREV_PAGE) },
       });
-      expect(instance.updatePage).toHaveBeenCalledWith(DEFAULT_PAGE - 1);
+      expect(instance.updatePage).toHaveBeenCalledWith(INIT_PAGE - 1);
     });
 
     it("should call 'updatePage' with decreased value", () => {
       instance.updatePage = jest.fn();
-      instance.setState({ page: DEFAULT_PAGE });
+      instance.setState({ page: INIT_PAGE });
       instance.handlePageChange({
         target: { getAttribute: jest.fn().mockReturnValue(NEXT_PAGE) },
       });
-      expect(instance.updatePage).toHaveBeenCalledWith(DEFAULT_PAGE + 1);
+      expect(instance.updatePage).toHaveBeenCalledWith(INIT_PAGE + 1);
     });
 
     it("should call 'updatePage' with incorrect value", () => {
       instance.updatePage = jest.fn();
-      instance.setState({ page: DEFAULT_PAGE });
+      instance.setState({ page: INIT_PAGE });
       instance.handlePageChange({
         target: { getAttribute: jest.fn().mockReturnValue('Test') },
       });
@@ -105,20 +105,20 @@ describe('Posts component', () => {
 
     it('should handle change of hits per page', () => {
       expect(component.state().hitsPerPage).toBe(defaultHitsPerPage);
-      instance.handleHitsChange({ target: { value: String(DEFAULT_PAGE) } });
-      expect(component.state().hitsPerPage).toBe(DEFAULT_PAGE);
+      instance.handleHitsChange({ target: { value: String(INIT_PAGE) } });
+      expect(component.state().hitsPerPage).toBe(INIT_PAGE);
     });
 
     it("should handle change page if 'Enter' clicked", () => {
-      instance.setState({ page: DEFAULT_PAGE });
+      instance.setState({ page: INIT_PAGE });
       instance.getSearch({ key: 'Enter' });
       expect(component.state().page).toBe(defaultPage);
     });
 
     it("should not change page if 'a' button clicked", () => {
-      instance.setState({ page: DEFAULT_PAGE });
+      instance.setState({ page: INIT_PAGE });
       instance.getSearch({ key: 'a' });
-      expect(component.state().page).toBe(DEFAULT_PAGE);
+      expect(component.state().page).toBe(INIT_PAGE);
     });
   });
 });
